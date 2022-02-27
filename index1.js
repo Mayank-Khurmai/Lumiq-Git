af0a48f7d10dbc8b6efbe234d7bbd94c875327b7
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2022-02-27 22:19:56 +0530

    Fixed the code in Feature2 branch

--- index1.js
+++ index1.js
@@ -1,12 +1,10 @@
 // JavaScript Function should be here
 
-<<<<<<< HEAD
 function feature2()
 {
-    console.log("This Function is made by User-2 in feature2 branch")
-=======
+    console.log("This Function is made by User-2 in feature2 branch");
+}
 function feature1()
 {
-    console.log("This Function is made by User-1 in feature1 branch")
->>>>>>> feature1
+    console.log("This Function is made by User-1 in feature1 branch");
 }
